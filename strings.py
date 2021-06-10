53065c6d81824376d1d943753275b0b7fcde4232
AnilKSahoo9 <asahoo112@gmail.com>
2021-06-10 20:55:23 +0530

    strings YT done

---
+++ strings.py
@@ -0,0 +1,69 @@
+# print('hello world')
+
+message = 'Hello Anil'
+
+# print(message)
+
+# my_message = 'hello kids'
+
+# print(my_message)
+
+# bobby_message = 'bobby\'s world'
+
+# print(bobby_message)
+
+# anil_message = "Anil's world"
+
+# print(anil_message)
+
+# multi_line = '''it is a
+# multi line'''
+
+# print(multi_line)
+
+# print(len(message))  # characters in a string
+
+# print(message[0])  # character in 0th index
+
+
+# # 1st parameter's character is included 2nd parameter's character is not includes
+# print(message[0:5])
+
+# print(message[0:])  # returns the string starting from 0th index till end
+
+# print(message[:5])  # returns the string upto 5th index
+
+# print(message.lower())  # returns the string in all lower case
+
+# print(message.upper())  # returns the string in all upper case
+
+# print(message.count('Hello'))  # no of time the string Hello is appearing
+
+# # returns the index from where the given string in the original one
+# print(message.find('Anil'))
+# # if the given string is not present then it will return -1
+
+# new_message = message.replace('Anil', 'World') #replace() returns a new string with replaced variable
+
+# print(new_message)
+
+greeting = 'hello'
+name = 'michael'
+
+# # greetings_name = greeting+', '+name  # formatted string
+
+# greetings_name_edited = '{}, {}. welcome!'.format(greeting, name)
+
+# print(greetings_name_edited)
+
+# see fStrings python 3.8
+
+# new_greetings = f'{greeting}, {name}.welcome!!!!'
+
+# print(new_greetings)
+
+# name1 = 'anu'
+
+# print(dir(name1)) #see what are the methods can be applied to the given type
+
+print(help(str))
